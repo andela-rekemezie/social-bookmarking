81c7d58b31f212c607ef078216fde3fc914a0c01
Rowland <rowland@Rowland.local>
2016-04-10 09:10:55 +0100

    Add flash messages to template

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -2,9 +2,12 @@ from datetime import datetime
 from flask import Flask, flash, render_template, request, url_for, redirect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '28\xc0x\x04\xea#\xd7\xe2\xf2\x19-\xe9\x8b\x81\xa5\x86\x03v\xbe\x85\xfdm\x8a'
+app.config[
+    'SECRET_KEY'] = '28\xc0x\x04\xea#\xd7\xe2\xf2\x19-\xe9\x8b\x81\xa5\x86\x03v\xbe\x85\xfdm\x8a'
+
 
 class User:
+
     def __init__(self, firstname, lastname):
         self.firstname = firstname
         self.lastname = lastname
@@ -13,10 +16,19 @@ class User:
         return '{}. {}'.format(self.firstname[0], self.lastname[0])
 
 
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
+    
+    
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title="Social Bookmarking", user=User('Rowland', 'Ekemezie'))
+    return render_template('index.html', title="Social Bookmarking", new_bookmarks=new_bookmarks(5))
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 
 bookmarks = []
@@ -24,9 +36,9 @@ bookmarks = []
 
 def store_bookmarks(url):
     bookmarks.append(dict(
-        date=datetime.utcnow(),
         url=url,
-        name='Rowland'
+        user='Rowland',
+        date=datetime.utcnow()
     ))
 
 
@@ -35,7 +47,7 @@ def add():
     if request.method == 'POST':
         url = request.form['url']
         store_bookmarks(url)
-        flash('Store url in {}'.format(url))
+        flash('Stored url in {}'.format(url))
         return redirect(url_for('index'))
     return render_template('add.html')
 
